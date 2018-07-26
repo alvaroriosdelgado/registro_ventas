721aa6eb12d42f9bdbd1dff6a4c9ab6aeb0f3293
Alvaro Rios <alvaro@smartsites.cl>
2018-07-26 12:47:50 -0400

    App Registro Ventas

---
+++ app/controllers/sales_controller.rb
@@ -0,0 +1,25 @@
+class SalesController < ApplicationController
+  def index
+    @sales = Sale.all
+  end
+
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+    @sale.save
+    redirect_to sales_path
+  end
+
+  def show
+    @sale = Sale.find(params[:id])
+  end
+
+  private
+
+  def sale_params
+    params.require(:sale).permit(:cod, :detail, :category, :value, :discount, :tax)
+  end
+end
